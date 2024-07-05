0483b27e60688d7758583342e89d1e24bce3437e
MrRetard <retardation213@gmail.com>
2024-07-05 15:28:26 +0700

    change output method

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from PyPDF2 import PdfReader, PdfWriter, PdfMerger
 
 def main():
     inp_path = './input/'
+    otp_path = './result/'
+    os.makedirs(inp_path, exist_ok=True)
     # inp_dir_list = sys.argv()
     inp_dir_list = os.listdir(inp_path)
+    if len(inp_dir_list) == 0:
+        return
     pdfMerger = PdfMerger()
-    filename = ''
     for i,v in enumerate(inp_dir_list):
         if v.endswith('.pdf'):
-            filename += v.split('.')[0] + ("_" if i < len(inp_dir_list)-1 else "")
             pdfMerger.append(inp_path+v)
-    pdfMerger.write(open('./result/{}.pdf'.format(filename),'wb'))
+    pdfMerger.write(open('result.pdf','wb'))
     return
 
 if __name__ == '__main__':
